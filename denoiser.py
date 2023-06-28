f82a357e8dc59b7471825f48fb6ef781b557d6de
Paul <pauhull3@gmail.com>
2023-06-28 22:05:43 +0200

    CPU denoiser

--- denoiser.py
+++ denoiser.py
@@ -6,21 +6,19 @@ from stft import STFT
 class Denoiser(torch.nn.Module):
     """ WaveGlow denoiser, adapted for HiFi-GAN """
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
     def __init__(
-        self, hifigan, filter_length=1024, n_overlap=4, win_length=1024, mode="zeros"
+            self, hifigan, filter_length=1024, n_overlap=4, win_length=1024, mode="zeros"
     ):
         super(Denoiser, self).__init__()
         self.stft = STFT(
             filter_length=filter_length,
             hop_length=int(filter_length / n_overlap),
             win_length=win_length,
-        ).to(Denoiser.device)
+        )
         if mode == "zeros":
-            mel_input = torch.zeros((1, 80, 88)).to(Denoiser.device)
+            mel_input = torch.zeros((1, 80, 88))
         elif mode == "normal":
-            mel_input = torch.randn((1, 80, 88)).to(Denoiser.device)
+            mel_input = torch.randn((1, 80, 88))
         else:
             raise Exception("Mode {} if not supported".format(mode))
 
@@ -31,7 +29,7 @@ class Denoiser(torch.nn.Module):
         self.register_buffer("bias_spec", bias_spec[:, :, 0][:, :, None])
 
     def forward(self, audio, strength=0.1):
-        audio_spec, audio_angles = self.stft.transform(audio.to(Denoiser.device).float())
+        audio_spec, audio_angles = self.stft.transform(audio.float())
         audio_spec_denoised = audio_spec - self.bias_spec * strength
         audio_spec_denoised = torch.clamp(audio_spec_denoised, 0.0)
         audio_denoised = self.stft.inverse(audio_spec_denoised, audio_angles)
